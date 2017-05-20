18ad9fcd1d71052d5b136caffd7f15f6e9c9de4e
Leyan529 <u0151057@nkfust.edu.tw>
2017-05-20 21:26:12 +0800

    PendingIntent 點選通知回到主頁面(畫面不重啟)

--- app/src/main/java/tw/edu/nkfust/eHat/NotificationHelper.java
+++ app/src/main/java/tw/edu/nkfust/eHat/NotificationHelper.java
@@ -1,7 +1,9 @@
 package tw.edu.nkfust.eHat;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.RingtoneManager;
 
@@ -12,21 +14,29 @@ import android.media.RingtoneManager;
 public class NotificationHelper {
     private Context context;
     private Notification notify;
-    public static int NOTIFY_MAIN= 1;
+    public static int NOTIFY_MAIN = 1;
 
 
     public NotificationHelper(Context context) {
         this.context = context;
     }
-    public Notification mainNotify(Bitmap bmp){
+
+    public Notification mainNotify(Bitmap bmp) {
+        Intent intent = new Intent(context,this.context.getClass());
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        /**getActivity(Context , 辨識碼 , Intent物件 , PendingIntent處理模式)*/
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         notify = new Notification.Builder(context)
                 .setTicker("eHat啟動中...")
-                .setSmallIcon(R.drawable.notifyp32,0)
+                .setSmallIcon(R.drawable.notifyp32, 0)
                 .setContentTitle("eHat")
-                .setContentText("功能畫面")
+                .setContentText("回到主頁面")
                 .setLargeIcon(bmp)
                 .setWhen(System.currentTimeMillis())
                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(pendingIntent)
                 .build();
         return notify;
     }
